aadc7684367b5c6a4e6a5f1dfab5cda5a58f5a84
jarb <blueciola@gmail.com>
2021-10-22 20:56:03 -0400

    step 1 logo & svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import './styles.css'
+import IMAGE from './react.png'
+import LOGO from './react.svg'
+
 export const App = () =>{
-    return <h1>React typescript </h1>
+    return <>
+     <h1>React typescript </h1>
+     <img src={IMAGE} alt="React Logo" width='300' height='200' />
+     <img src={LOGO} alt="React Logo" width='300' />
+
+    </>
 }
