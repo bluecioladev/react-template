5d51f163b94de570be145f53fbd472b25e5aca3a
jarb <blueciola@gmail.com>
2021-10-27 20:34:35 -0400

    husky and lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './styles.css'
 import IMAGE from './react.png'
 import LOGO from './react.svg'
+import { ClickCounter } from './ClickCounter'
 
-export const App = () =>{
-    return <>
-     <h1>React typescript </h1>
-     <img src={IMAGE} alt="React Logo" width='300' height='200' />
-     <img src={LOGO} alt="React Logo" width='300' />
-
+export const App = () => {
+  const name = 'jarb'
+  return (
+    <>
+      <h1>
+        {' '}
+        hello jarb editado React typescript - {process.env.NODE_ENV} -{name}
+        {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React Logo" width="300" height="200" />
+      <img src={LOGO} alt="React Logo" width="300" height="200" />
+      <ClickCounter />
     </>
+  )
 }
